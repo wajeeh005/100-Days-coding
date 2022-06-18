c280de14ba6cf35ce3da90e4e380b62056f63092
wajeeh005 <63997808+wajeeh005@users.noreply.github.com>
2022-06-18 23:44:00 +0500

    Add files via upload

---
+++ Day 2/README.md
@@ -0,0 +1,13 @@
+# Tip Calculator
+
+### If the bill was $150.00, split between 5 people, with 12% tip.
+
+### Each person should pay (150.00 / 5) * 1.12 = 33.6
+
+### Format the result to 2 decimal places = 33.60
+
+### Thus everyone's share of the total bill is $30.00 plus a $3.60 tip.
+
+#### Hint:
+1.  Tip is total bill * percentage of tip
+2.  Each person pay total bill + tip  / total no of split
--- Day 2/Tip_calculator.py
+++ Day 2/Tip_calculator.py
@@ -1,15 +1,10 @@
-# Tip calcultor
-#1. First greeting
-# 2.  What was the total bill? ask from User
-# 3.  What percentage tip would you like to give?
-# 4.  give 3 options 10,12 or 15 
-#         user can put any one of them 
-# 5.  Ask How many people to split the bill?
-# 6.  divide the whole bill on given n
+def tip_calculator():
+    print("Hello Welcome to Tip calculator")
+    total_bill =int(input("What is your total bill?     "))
+    percentage_of_bill= int(input("What percentage you like? 10,12 or 15     "))
+    total_no_of_peoples_to_split =int(input("how many peoples to split the bill?        "))
+    bill= round((total_bill + total_bill * (percentage_of_bill/100))/total_no_of_peoples_to_split)
+    print(f"Each person should pay: ${bill}")
 
+tip_calculator()
 
-print("Hello Welcome to Tip calculator")
-total_bill =int(input("What is your total bill?"))
-percentage_of_bill= input("Wat percentage you like? 10,12 or 15")
-total_no_of_peoples_to_split =int(input("how many peoples to split the bill?"))
-bill= (total_bill * (percentage_of_bill/100))/total_no_of_peoples_to_split
