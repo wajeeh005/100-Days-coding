cea86fc83bc0551d10eb7dc12a73189845a6f256
wajeeh005 <63997808+wajeeh005@users.noreply.github.com>
2022-06-28 22:57:15 +0500

    updated files

--- Day 9/Secret Auction Program/main.py
+++ Day 9/Secret Auction Program/main.py
@@ -1,39 +1,26 @@
-from replit import clear
 from art import logo
 print(logo)
 
-bids = {}
-bidding_finished = False
+bid_dic={}
+x = True
 
-def find_highest_bidder(bidding_record):
-  highest_bid = 0
-  winner = ""
-  # bidding_record = {"Angela": 123, "James": 321}
-  for bidder in bidding_record:
-    bid_amount = bidding_record[bidder]
-    if bid_amount > highest_bid: 
-      highest_bid = bid_amount
-      winner = bidder
-  print(f"The winner is {winner} with a bid of ${highest_bid}")
-
-while not bidding_finished:
-  name = input("What is your name?: ")
-  price = int(input("What is your bid?: $"))
-  bids[name] = price
-  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
-  if should_continue == "no":
-    bidding_finished = True
-    find_highest_bidder(bids)
-  elif should_continue == "yes":
-    clear()
+def highest_bid(bid_dictionary):
+  highest_bid_value=0
+  winner=""
+  for i in bid_dictionary:
+    amount=bid_dictionary[i]
+    if highest_bid_value < amount:
+      highest_bid_value=amount
+      winner=i
+  print(f"{winner} is the winner with the highest bid of {highest_bid_value}$.")
   
+  
+while x:
+  name = input("Enter your name:  ")
+  bid_price = int(input("Enter your bid prize: $ "))
+  bid_dic[name]= bid_price
+  for_countinue=input("Are there any other bidders? Type 'yes or 'no':    ").lower()
+  if for_countinue=="no":
+    x=False
+    highest_bid(bid_dic)
 
-"""
-FAQ: My console doesn't clear()
-
-This will happen if you’re using an IDE other than replit. 
-I’ll cover how to use PyCharm in Day 15. That said, you can write your own clear() function or configure your IDE like so: 
-
-https://www.udemy.com/course/100-days-of-code/learn/lecture/19279420#questions/16084076
-
-"""
