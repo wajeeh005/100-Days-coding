df8de0225644d1f6ab8ea13dedc58924516a2565
wajeeh005 <63997808+wajeeh005@users.noreply.github.com>
2022-07-02 00:28:12 +0500

    Add files via upload

---
+++ Day 14/Lower higher game/README.md
@@ -0,0 +1,17 @@
+# Write a program of Higher Lower Game.
+## First you should go (**HERE**)[http://www.higherlowergame.com/]  and play the game.
+
+
+### Steps to Follow:
+
+1. Generate a random players data from the game data for player A and B.
+
+2. Print in this format 'Compare A:  Cristiano Ronaldo , a Footballer , from Portugal'
+    "Against B:  Taylor Swift , a Musician , from United States"
+
+3. Ask user for a guess which have more Followers A or B.
+
+4. Check if user is correct update the score and again start and make user choice a player A for next comparision.
+
+5. If user is incorrect clear screen and show Final score of user.
+
--- Day 14/Lower higher game/main.py
+++ Day 14/Lower higher game/main.py
@@ -1,46 +1,43 @@
-from winsound import PlaySound
-
-from sklearn.metrics import SCORERS
+from replit import clear
 from game_data import data
 from art import logo ,vs
 import random
 
-
-print(logo)
-def who_win(user_ask,player_A,player_B):
-    if user_ask =="a":
-        if player_A['follower_count']> player_B['follower_count']:
-            print("A win")
-            return True
-    elif user_ask=="b":
-        if player_B['follower_count']> player_A['follower_count']:
-            print("B win")
-            return True
-    return False
-
-
 def random_player(data):
     return random.choice(data)
-def higher_lower(score1):
-    score=score1
-    player_A= random_player(data)
+
+
+def who_win(user_ask,player_A,player_B):
+    if player_A['follower_count']> player_B['follower_count']:
+        return user_ask == 'a'    
+    else:
+        return user_ask=="b"
+
+def higher_lower():
+    print(logo)
+    score=0
     player_B=random_player(data)
-    print(f"Compare A:  {player_A['name']} , a {player_A['description']} , from {player_A['country']}")
-    print(vs)
+    end_the_game=True
+    while end_the_game:
+        player_A= player_B
+        player_B=random_player(data)
+        while player_A==player_B:
+            player_B=random_player(data)
 
-    print(f"Against B:  {player_B['name']} , a {player_B['description']} , from {player_B['country']}")
+        print(f"Compare A:  {player_A['name']} , a {player_A['description']} , from {player_A['country']}")
+        print(vs)
 
-    user_ask=input("Who has more Followers? Type 'A' or 'B':     ").lower()
-    print(player_A['follower_count'],"A")
-    print(player_B['follower_count'],"B")
-    if who_win(user_ask,player_A,player_B):
-        score+=1
-        higher_lower(score)
-    return scoring(score)
+        print(f"Against B:  {player_B['name']} , a {player_B['description']} , from {player_B['country']}")
+        # print(player_A['follower_count'],"A")
+        # print(player_B['follower_count'],"B")
+        user_ask=input("Who has more Followers? Type 'A' or 'B':     ").lower()
+        clear()
+        print(logo)
+        if who_win(user_ask,player_A,player_B):
+            score+=1
+            print(f"You're right. ")
+        else:
+            end_the_game=False
+            print(f"Sorry, That's wrong. Your Final Scores are {score}")
 
-def scoring(score):
-    score+=1
-    return print(score)
-
-score=0
-higher_lower(score)
+higher_lower()
