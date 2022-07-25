9092dc54d494ff36c509844815d3c134365d96e9
wajeeh005 <63997808+wajeeh005@users.noreply.github.com>
2022-07-25 23:53:11 +0500

    API post, put and delete requests & Headers

---
+++ Day 37/Habbit Tracker/README.md
@@ -0,0 +1,19 @@
+# Habit Tracking Project
+This is basically a habit tracking project as shown from name in which we used API to post, update and delete. It's an  online so you can daily add your habit details and easily track the record of yourself.
+
+## Steps To follow
+1. You have to go on [Pixela](https://pixe.la/).  
+Pixela is the API service. With this service, you can get a GitHub like graph that expresses the degree of your daily various activities on a basis with a vivid gradation. All operations are performed by API. And, it's free.
+    
+
+2. Then go to [Documentation](https://docs.pixe.la/) where you can understand how to create a user first.
+3. How to create a new user? Go on[post-user](https://docs.pixe.la/entry/post-user).
+4. How to create Grpah? Go on [post-graph](https://docs.pixe.la/entry/post-graph).
+5. How to post a pixel on the Graph? Go on [post-pixel](https://docs.pixe.la/entry/post-pixel).
+6. How to update a pixel? Go on [put-pixel](https://docs.pixe.la/entry/put-pixel).
+7. How to delete a pixel? Go on [delete-pixel](https://docs.pixe.la/entry/delete-pixel)
+
+
+
+### Note:
+You simply uncomment the lines of requestes according to you and it will work. For example if you want to update then uncomment update line and fill the date according to you and then request.
---
+++ Day 37/Habbit Tracker/main.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+import requests
+
+TOKEN = "s2jkl2l1n3af14v9asd8fa12fv9fa8s7jh12u3o0d"
+USERNAME = "Your_name"
+pixela_endpoint = "https://pixe.la/v1/users"
+parameters = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+# #
+# response = requests.post(url=pixela_endpoint, json=parameters)
+# print(response)
+ID="graph1"
+habit_name="Your habbit name"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_prams = {
+    "id": ID,
+    "name": habit_name,
+    "unit": "Km",
+    "type": "float",
+    "color": "sora",
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_prams, headers=headers)
+# print(response.text)
+
+pixel_endpoint=f"{graph_endpoint}/{ID}"
+today=datetime.now()
+
+pixel_params={
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "15.1",
+}
+# pixel_response=requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
+# print(pixel_response.text)
+
+#Pixel Updating using put
+date_for_update="20220723"
+updating_endpoint=f"{pixel_endpoint}/{date_for_update}"
+update_params={
+    "quantity": "12.3"
+}
+# update_response=requests.put(url=updating_endpoint,json=update_params,headers=headers)
+# print(update_response.text)
+
+#deleteing a pixel
+
+date_for_deleting="20220723"
+deleting_endpoint=f"{pixel_endpoint}/{date_for_update}"
+deleting_response=requests.delete(url=deleting_endpoint,headers=headers)
+print(deleting_response)
