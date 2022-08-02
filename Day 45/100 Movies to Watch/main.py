d742fbf0eeb8fd0c4e1ddc22461c318f25199739
wajeeh005 <63997808+wajeeh005@users.noreply.github.com>
2022-08-02 23:59:22 +0500

    Web Scrapping using BeautiSoup

--- Day 45/100 Movies to Watch/main.py
+++ Day 45/100 Movies to Watch/main.py
@@ -15,7 +15,7 @@ all_movies = soup.find_all(name="h3", class_="title")
 movie_titles = [movie.getText() for movie in all_movies]
 movies = movie_titles[::-1]
 
-with open("movies.txt", mode="w") as file:
+with open("movies.txt", mode="w",encoding="utf-8") as file:
     for movie in movies:
         file.write(f"{movie}\n")
 
